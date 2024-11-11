64a2062626904ae8920eecbbf593ffd204d73a25
emreertugrl <emrertugrl@gmail.com>
2024-11-11 11:59:53 +0300

    Hero component complete

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,5 +1,30 @@
+import { motion } from "framer-motion";
+import Button from "../button";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="pt-10 padding-x flex-1 max-h-[920px]">
+        <h1 className="hero__title">Özgürlüğü Hisset, Yolculuğa Başla</h1>
+        <p className="hero__subtitle">
+          Altın standartta hizmetle unutulmaz bir yolculuğa hazır mısın? Araç
+          kiralama deneyimini Altın Seçenekleri ile taçlandırarak her anını özel
+          kılabilirsin.
+        </p>
+        <Button title="Arabaları Keşfet" designs="mt-10"></Button>
+      </div>
+      <div className="flex justify-center">
+        <motion.img
+          initial={{ translateX: 200, scale: 0.7, opacity: 0 }}
+          animate={{ translateX: 0, scale: 1, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          src="/hero.png"
+          alt="gray bmw"
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
