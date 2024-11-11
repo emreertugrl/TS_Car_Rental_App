c54cf6c9e6cc865c9d7eccd78b7aa18d01dc07c6
emreertugrl <emrertugrl@gmail.com>
2024-11-11 11:33:21 +0300

    app bg color defined

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/hero";
 
 const App = () => {
   return (
-    <div className="bg-black-100 text-white min-h-screen">
+    <div className="bg-[rgb(23,23,23)] text-white min-h-screen">
       <Header />
       <Hero />
     </div>
