e345670a14474298e5d740a1b6ec77d4c728c13c
emreertugrl <emrertugrl@gmail.com>
2024-11-12 17:55:33 +0300

    update images with generateImages

--- src/components/modal/Image.tsx
+++ src/components/modal/Image.tsx
@@ -1,3 +1,4 @@
+import generateImage from "../../utils/generateImage";
 import { CarType } from "../../utils/types";
 
 const Image = ({ car }: { car: CarType }) => {
@@ -6,27 +7,31 @@ const Image = ({ car }: { car: CarType }) => {
     <div className="flex-1 flex-col gap-3">
       {/* Büyük Resim */}
       <div className="w-full h-40 bg-pattern bg-center rounded-lg">
-        <img src="hero.png" alt="" className="h-full mx-auto object-contain" />
+        <img
+          src={generateImage(car)}
+          alt=""
+          className="h-full mx-auto object-contain"
+        />
       </div>
       {/* Küçük Resim */}
       <div className="flex gap-3 my-3">
         <div className="rounded flex-1 flex  relative h-24 bg-primary-blue-100">
           <img
-            src="hero.png"
+            src={generateImage(car, "29")}
             alt=""
             className="mx-auto object-contain min-w-[146px]:"
           />
         </div>
         <div className="rounded flex-1 flex  relative h-24 bg-primary-blue-100">
           <img
-            src="hero.png"
+            src={generateImage(car, "2")}
             alt=""
             className="mx-auto object-contain min-w-[146px]:"
           />
         </div>
         <div className="rounded flex-1 flex  relative h-24 bg-primary-blue-100">
           <img
-            src="hero.png"
+            src={generateImage(car, "13")}
             alt=""
             className="mx-auto object-contain min-w-[146px]:"
           />
