a5305c88287660e69d7c3728b7fdd916f28e95e2
emreertugrl <emrertugrl@gmail.com>
2024-11-13 11:45:46 +0300

    searchBar defined

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -3,9 +3,9 @@ import { makes } from "./../../utils/constants";
 import { FormEvent, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 const SearchBar = () => {
-  const [make, setMake] = useState<string>("");
-  const [model, setModel] = useState<string>("");
   const [params, setParams] = useSearchParams();
+  const [make, setMake] = useState<string>(params.get("make") || "");
+  const [model, setModel] = useState<string>(params.get("model") || "");
 
   const options = useMemo(
     () =>
@@ -18,10 +18,13 @@ const SearchBar = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // marka ve model url arama parametresi olarak ekle
-    setParams({
-      make: make.toLowerCase(),
-      model: model.toLowerCase(),
-    });
+    setParams({ make, model });
+  };
+
+  // urldeki seçili markayı al
+  const selected = {
+    label: params.get("make") || "",
+    value: params.get("make") || "",
   };
   return (
     <form onSubmit={handleSubmit} className="searchbar gap-4">
@@ -31,6 +34,7 @@ const SearchBar = () => {
           className="w-full text-black"
           options={options}
           placeholder="Marka Seçiniz..."
+          defaultValue={selected}
         />
         <button className="ml-3 sm:hidden">
           <img src="/search.svg" className="size-10" alt="" />
@@ -48,6 +52,7 @@ const SearchBar = () => {
           className="searchbar__input rounded text-black"
           placeholder="Örn: Mondeo"
           onChange={(e) => setModel(e.target.value)}
+          defaultValue={params.get("model") || ""}
         />
         <button className="ml-3">
           <img src="/search.svg" className="size-10" alt="" />
